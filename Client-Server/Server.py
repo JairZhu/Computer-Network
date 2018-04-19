b570c53dde4d4e65eb4d94925c5d510e9f0cb3ae
Jair Zhu <739741104@qq.com>
2018-04-19 22:16:52 +0800

    更新应用层协议，增加下载进度条显示

--- Client-Server/Server.py
+++ Client-Server/Server.py
@@ -1,32 +1,68 @@
 import struct
 import socket
 import os
+import time
+from tqdm import tqdm
+
+list_file = 0
+transmission = 1
+get = 2
+version = 1
+head_length = struct.calcsize('bbQ')
+files = [b'test.jpg', b'test.mp4']
+
+def makehead(version, service_number, length):
+    return struct.pack('bbQ', version, service_number, length)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('172.18.35.96', 8100))
 server.listen(1)
-print("Waiting for client's request...")
 
 while True:
+    print("Waiting for connection...")
     connection, addr = server.accept()
     print("Connect IP:", addr[0], "Port:", addr[1])
-    request = connection.recv(struct.calcsize('8s')).decode('utf-8')
-    print("Receive", request, "request")
-    if request == 'GET FILE':
-        connection.send('test.mkv'.encode('utf-8'))
-        connection.send(struct.pack('Q', os.stat('test.mkv').st_size))
-        print("Sending", os.stat('test.mkv').st_size, "bytes test.mkv...")
 
-        file = open('test.mkv', 'rb')
-        sendsize = 0
+    connection.send(makehead(version, list_file, struct.calcsize('bbQ8sQ8sQ')))
+    for file in files:
+        connection.send(struct.pack('8sQ', file, os.stat(file).st_size))
+
+    headinfo = connection.recv(head_length)
+    Version, request_number, length = struct.unpack('bbQ', headinfo)
+    if request_number == get:
+        file_name = connection.recv(length - head_length).decode()
+        file_length = os.stat(file_name).st_size
+        print('Client request: Get', file_name)
+        print('Start sending', file_length, 'bytes', file_name)
+        time.sleep(1)
+
+        connection.send(makehead(version, transmission, head_length + file_length))
+
+        file_info = open(file_name, 'rb')
+        t = tqdm(total=file_length, unit='bytes')
+        sent_size = 0
         while True:
-            data = file.read(2 ** 23)
-            if not data:
-                print('file send over')
+            data = b''
+            if file_length - sent_size > 2 ** 20:
+                data = file_info.read(2 ** 20)
+                sent_size += len(data)
+                t.update(len(data))
+                connection.send(data)
+            elif file_length - sent_size > 1024:
+                data = file_info.read(2 ** 17)
+                sent_size += len(data)
+                t.update(len(data))
+                connection.send(data)
+            else:
+                data = file_info.read(1024)
+                sent_size += len(data)
+                t.update(len(data))
+                connection.send(data)
+
+            if sent_size >= file_length:
+                t.close()
                 break
 
-            sendsize += len(data)
-            connection.send(data)
-            print('Have sent', sendsize, "bytes")
-
-    connection.close()
+        file_info.close()
+        connection.close()
+        print(file_name, 'send over', '\nConnection close')
